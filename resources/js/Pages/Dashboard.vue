b9f61581e85048f0d67cddec94c3f99435952dfb
Excel Dwi Oktavianto <excelokta@gmail.com>
2021-11-01 02:53:30 +0700

    Tampilah Homepage

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -9,37 +9,14 @@
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="w-full grid grid-cols-1 md:grid-cols-2 gap-6">
-
-                    <div class="bg-white border border-gray-200 hover:border-blue-600 rounded-lg p-6">
+                    <div class="bg-white border border-gray-200 hover:border-blue-600 rounded-lg p-6" v-for="data in video">
                         <div class="flex items-center space-x-6 mb-4">
                             <img class="w-full h-56 object-cover object-center rounded" 
-                            src="https://images.unsplash.com/photo-1499952127939-9bbf5af6c51c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1176&q=80" alt="photo">
+                            :src="'/thumbnails/'+data.thumbnail" alt="photo">
                         </div>
                         <div>
-                            <a href="#" class="text-blue-600 font-bold text-xl">Judul Videonya</a>
-                            <p class="text-gray-700">20-20-2021</p>
-                        </div>
-                    </div>
-
-                    <div class="bg-white border border-gray-200 hover:border-blue-600 rounded-lg p-6">
-                        <div class="flex items-center space-x-6 mb-4">
-                            <img class="w-full h-56 object-cover object-center rounded" 
-                            src="https://images.unsplash.com/photo-1499952127939-9bbf5af6c51c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1176&q=80" alt="photo">
-                        </div>
-                        <div>
-                            <a href="#" class="text-blue-600 font-bold text-xl">Judul Videonya</a>
-                            <p class="text-gray-700">20-20-2021</p>
-                        </div>
-                    </div>
-
-                    <div class="bg-white border border-gray-200 hover:border-blue-600 rounded-lg p-6">
-                        <div class="flex items-center space-x-6 mb-4">
-                            <img class="w-full h-56 object-cover object-center rounded" 
-                            src="https://images.unsplash.com/photo-1499952127939-9bbf5af6c51c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1176&q=80" alt="photo">
-                        </div>
-                        <div>
-                            <a href="#" class="text-blue-600 font-bold text-xl">Judul Videonya</a>
-                            <p class="text-gray-700">20-20-2021</p>
+                            <jet-nav-link :href="route('video.watch', data.id)" class="text-blue-600 font-bold text-xl">{{ data.name }}</jet-nav-link>
+                            <p class="text-gray-700">{{ data.created_at }}</p>
                         </div>
                     </div>
                 </div>
@@ -51,10 +28,13 @@
 <script>
     import { defineComponent } from 'vue'
     import AppLayout from '@/Layouts/AppLayout.vue'
+    import JetNavLink from '@/Jetstream/NavLink.vue'
 
     export default defineComponent({
+        props: ['video'],
         components: {
             AppLayout,
+            JetNavLink,
         },
     })
 </script>
